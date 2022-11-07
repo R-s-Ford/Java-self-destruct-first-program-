c8aba861e827b29214b06cf6a62fa019fed0cfb9
R-s-Ford <ricky.s.ford@hotmail.com>
2022-11-07 21:39:08 +1300

    test

--- scipt.js
+++ scipt.js
@@ -1,12 +1,10 @@
 
 alert("Warning! This message will self-destruct in");
 
-
 alert("3");
 alert("2");
 alert("1");
 
 document.querySelector("h1").textContent = "🔥BOOM!🔥";
 
-
-console.log("Message destroyed!");
+console.log("Message destroyed!")
